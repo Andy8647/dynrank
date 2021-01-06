a38adcf9645de8262f379a4e69b0f8dd42a70b21
Andy8647 <andy8647lgc@gmail.com>
2021-01-06 13:00:39 -0500

    typo fixed

--- server.js
+++ server.js
@@ -9,19 +9,10 @@ const path = require('path');
 app.use(express.static(path.join(__dirname, '/pub')))
 
 
-
-
 app.get('/', (req, res) => {
 	res.sendFile(__dirname + '/pub/pages/index.html')
 })
 
-// app.get('/doc', (req, res) => {
-// 	res.sendFile(__dirname + '/pub/pages/documentation.html')
-// })
-
-// app.get('/exp', (req, res) => {
-// 	res.sendFile(__dirname + '/pub/pages/examples.html')
-// })
 
 const port = process.env.PORT || 5000
 app.listen(port, () => {
